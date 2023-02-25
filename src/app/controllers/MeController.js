620fcc52eee3cc1f45157f1336d355437b2c4756
devboy2011 <buituananhit.contact@gmail.com>
2023-02-25 18:52:31 +0700

    Sort Middleware

--- src/app/controllers/MeController.js
+++ src/app/controllers/MeController.js
@@ -8,7 +8,15 @@ class MeController {
     // [GET] /me/stored/courses
     courses(req, res, next) {
     
-        Promise.all([Course.find({}), Course.countDocumentsDeleted()])
+        let courseQuery = Course.find({});
+        
+        if (req.query.hasOwnProperty('_sort')) {
+            courseQuery = courseQuery.sort({
+                [req.query.column]: req.query.type
+            })
+        }
+    
+        Promise.all([courseQuery, Course.countDocumentsDeleted()])
             .then (([courses, deletedCount]) => 
                 res.render('me/courses', {
                     deletedCount,
--- src/index.js
+++ src/index.js
@@ -12,6 +12,8 @@ const port = 3000;
 const route = require('./routes/index');
 const db = require('./config/db')
 
+const SortMiddleware = require('./app/middlewares/SortMiddleware')
+
 // connect to db
 db.connect()
 
@@ -21,6 +23,28 @@ const hbs = create({
     extname : "handlebars",
     helpers : {
         sum : (a, b) => a + b,
+        sortable : (field, sort) => {
+        
+            const sortType = field === sort.column? sort.type : 'default'
+        
+            const icons = {
+                default : "bi bi-filter-square",
+                asc : "bi bi-sort-up",
+                desc : "bi bi-sort-down"
+            }
+            const types = {
+                default : 'asc',
+                asc : 'desc',
+                desc : 'asc'
+            }
+            
+            const icon = icons[sortType]
+            const type = types[sortType]
+            
+            return `<a href="?_sort&column=${field}&type=${type}">
+            <i class="${icon}"></i>
+          </a>`
+        }
     }
 })
 
@@ -28,6 +52,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan('combined'))
 app.use(methodOverride('_method'));
+app.use(SortMiddleware)
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
